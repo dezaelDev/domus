ad48aa3c4100ca788645605bd0878fb3c42a51a7
dezaelDev <emmanuelfernandez.developer@gmail.com>
2022-11-20 15:50:20 -0300

    se ejecuta express

--- src/index.js
+++ src/index.js
@@ -1,28 +1,7 @@
-const sql = require('mssql');
-const sqlConfig = {
-	user: process.env.DB_USER,
-	password: process.env.DB_PWD,
-	database: process.env.DB_NAME,
-	server: '127.0.0.1',
-	pool: {
-		max: 10,
-		min: 0,
-		idleTimeoutMillis: 30000,
-	},
-	options: {
-		instancename: 'SQLEXPRESS',
-	},
-};
+import app from './app';
 
-async () => {
-	try {
-		// make sure that any items are correctly URL encoded in the connection string
-		await sql.connect(sqlConfig);
-		const result =
-			await sql.query`select * from mytable where id = ${value}`;
-		console.dir(result);
-	} catch (err) {
-		// ... error checks
-		console.error(err);
-	}
-};
+app.listen(app.get('port'), (err) => {
+	if (err) console.error(err);
+
+	console.log('server on port 3000');
+});
